1efcc164d37b1f76c7f9fe4969a0d4ac45c5317c
chengxinya <3115693395@qq.com>
2024-11-22 23:08:54 +0800

    员工分页

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,12 +1,15 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface EmployeeMapper {
+public interface
+EmployeeMapper {
 
     /**
      * 根据用户名查询员工
@@ -21,8 +24,15 @@ public interface EmployeeMapper {
      * @param employee
      */
 
-    @Insert("insert into employee (name,username,password,phone,sex,id_number,create_time,create_user,update_user,status) " +
+    @Insert("insert into employee (name,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status) " +
             "values" +
-            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{createUser},#{updateUser},#{status})")
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
